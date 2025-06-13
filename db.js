8ace4a7dfa3c7db265217de9c9d93ddfb8407723
Harsh Sharma <krish9515274@gmail.com>
2025-06-13 11:02:50 +0530

    Step-5, added the schema

---
+++ db.js
@@ -0,0 +1,55 @@
+const {Schema, default:mongoose} = require("mongoose");
+console.log("connected to db");
+mongoose.connect("mongodb+srv://krish9515274:K_5h4rm417dec@cluster0.6mwwk9o.mongodb.net/coursera-app");
+
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+const userSchema = new Schema({
+    email: {type:String, unique:true},
+    password: String,
+    firstName: String,
+    lastname: SVGFESpecularLightingElement
+
+});
+
+
+const adminSchema = new Schema({
+    email: {type: String, unique:true},
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+
+});
+
+
+const purchaseSchema = new Schema({
+    userId:ObjectId,
+    courseId:ObjectId
+});
+
+
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
+
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course")
 const app = express();
 
-
 app.use("/api/v1/user", userRouter);
 app.use("api/v1/admin", userRouter);
 app.use("/api/v1/course", courseRouter);
